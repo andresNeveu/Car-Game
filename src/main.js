97a6b7cbcc972fd3ca5048b944ab95a6f6e0592f
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-01-31 16:37:09 -0500

    movements

--- src/main.js
+++ src/main.js
@@ -130,3 +130,38 @@ const getVehicleSpeed = type => {
 		return minimumSpeed + Math.random() * (maximumSpeed - minimumSpeed);
 	}
 };
+
+// movements
+
+const movePlayerCar = timeDelta => {
+	const playerSpeed = getPlayerSpeed();
+	playerAngleMoved -= playerSpeed * timeDelta;
+
+	const totalPlayerAngle = playerAngleInitial + playerAngleMoved;
+
+	const playerX = Math.cos(totalPlayerAngle) * trackRadius - arcCenterX;
+	const playerY = Math.sin(totalPlayerAngle) * trackRadius;
+
+	playerCar.position.x = playerX;
+	playerCar.position.y = playerY;
+
+	playerCar.rotation.z = totalPlayerAngle - Math.PI / 2;
+};
+
+const moveOtherVehicles = timeDelta => {
+	otherVehicles.forEach(vehicle => {
+		if (vehicle.clockwise) {
+			vehicle.angle -= speed * timeDelta * vehicle.speed;
+		} else {
+			vehicle.angle += speed * timeDelta * vehicle.speed;
+		}
+
+		const vehicleX = Math.cos(vehicle.angle) * trackRadius + arcCenterX;
+		const vehicleY = Math.sin(vehicle.angle) * trackRadius;
+		const rotation =
+			vehicle.angle + (vehicle.clockwise ? -Math.PI / 2 : Math.PI / 2);
+		vehicle.mesh.position.x = vehicleX;
+		vehicle.mesh.position.y = vehicleY;
+		vehicle.mesh.rotation.z = rotation;
+	});
+};
