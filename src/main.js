93c6269ffc79a4372463388742ab19b4789aca21
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-01-31 16:36:28 -0500

    speeds

--- src/main.js
+++ src/main.js
@@ -109,3 +109,24 @@ window.addEventListener('keyup', function (event) {
 		decelerate = false;
 	}
 });
+
+// speeds
+
+const getPlayerSpeed = () => {
+	if (accelerate) return speed * 2;
+	if (decelerate) return speed * 0.5;
+	return speed;
+};
+
+const getVehicleSpeed = type => {
+	if (type === 'car') {
+		const minimumSpeed = 1;
+		const maximumSpeed = 2;
+		return minimumSpeed + Math.random() * (maximumSpeed - minimumSpeed);
+	}
+	if (type === 'truck') {
+		const minimumSpeed = 0.6;
+		const maximumSpeed = 1.5;
+		return minimumSpeed + Math.random() * (maximumSpeed - minimumSpeed);
+	}
+};
